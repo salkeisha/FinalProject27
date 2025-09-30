34a6874dd7fc7497c32668ba64eed11c3184e9b6
aylafada <aylafada7.gmail.com>
2025-09-30 14:16:39 +0700

    iyo

--- Test.java
+++ Test.java
@@ -2,5 +2,7 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("Hai. ini baris kode pertama");
+    System.out.println("Hallo kei, aku ayla")
+        
     }
 }
